e578c472bfcff1e2872bc614a3b56033a99235f1
brewmer <brewmercode@gmail.com>
2026-04-30 21:52:00 -0700

    Added C++ to nvim-treesitter config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,11 +30,9 @@ vim.pack.add({
 --
 --Treesitter
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = { 'c++' },
+	pattern = { 'c++', 'lua' },
 	callback = function() 
 		vim.treesitter.start()
-		vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-		vim.wo[0][0].foldmethod = 'expr'
 	end
 })
 vim.lsp.enable('clangd')
